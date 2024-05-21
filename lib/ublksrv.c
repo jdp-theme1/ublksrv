f6cf18413c57e6e29e0361fc46e870a05fe1c2ea
kunchichiang <kunchichiang@mxic.com.tw>
2024-05-21 09:29:00 +0800

    fetch from DelayAdd

--- lib/ublksrv.c
+++ lib/ublksrv.c
@@ -204,16 +204,18 @@ int ublksrv_complete_io(const struct ublksrv_queue *tq, unsigned tag, int res)
 		case UBLK_IO_OP_DISCARD:
 			break;
 		case UBLK_IO_OP_READ:
-			if(s%99999 == 0) {usleep(500);}
-			else if(s%9999 == 0) {usleep(300);}
-			else if(s%999 == 0) {usleep(200);}
-			else if(s%99 == 0) {usleep(100);}
+			if(s%99999 == 0) usleep(1000);
+			else if(s%9999 == 0) usleep(500);
+			else if(s%999 == 0) usleep(300);
+			else if(s%99 == 0) usleep(200);
+			else usleep(50);
 			break;
 		case UBLK_IO_OP_WRITE: 
-			if(s%99999 == 0) {usleep(5000);}
-			else if(s%9999 == 0) {usleep(3000);}
-			else if(s%999 == 0) {usleep(2000);}
-			else if(s%99 == 0) {usleep(1000);}
+			if(s%99999 == 0) usleep(5000);
+			else if(s%9999 == 0) usleep(3000);
+			else if(s%999 == 0) usleep(2000);
+			else if(s%99 == 0) usleep(1000);
+			else usleep(100);
 			break;
 		default:
 			break;
